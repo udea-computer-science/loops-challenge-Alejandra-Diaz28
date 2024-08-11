a5ca1dfbdd38ed26fc99c63cb0277a5905e43b54
Alejandra-Diaz28 <160366614+Alejandra-Diaz28@users.noreply.github.com>
2024-08-11 11:47:29 -0500

    loops_ejercicio.py

--- challenges/loops/main.py
+++ challenges/loops/main.py
@@ -1,3 +1,13 @@
-if __name__ == '__main__':
+if __name__ == "__main__":
     n = int(input())
-    ...
+    print(f"el número ingresado es: {n}")
+    x = []
+    if 1 <= n and n <= 20:
+        for i in range(0, n):
+            x.append(i)
+        print(f"los números no negativos menores a {n} son: {x}")
+        print("el cuadrado de cada número es:")
+        for i in range(0, n):
+            print(i**2)
+    else:
+        print("el número debe estar entre 1 y 20")
